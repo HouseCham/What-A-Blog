db10f8b3fd06364fec785e7ddd01cdc240e79cfa
Ramses Rmz <a00344520@tec.mx>
2022-04-30 12:59:21 -0500

    some details

--- WEB-INF/pages/common/userCard.php
+++ WEB-INF/pages/common/userCard.php
@@ -2,13 +2,6 @@
     <div class="user_section">
         <h3 class="user_welcome text-center mt-1">Welcome back!!</h3>
         <h4 class="username text-center"><?= $_SESSION['user']['name']; ?></h4>
-
-        <ul class="user_list">
-            <li class="user_info">
-                <i class="uil uil-notes user_icon"></i>
-                <span class="user_fact">Total of articles: </span>
-            </li>
-        </ul>
         <!-- Edit profile -->
         <?php include_once 'editProfile.php' ?>
         <!-- New Article -->
